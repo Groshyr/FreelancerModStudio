ab1bd85da4853bde69ccf4831095628e78735c07
Anton Panasiuk <164844553+Groshyr@users.noreply.github.com>
2026-07-06 07:36:50 +0200

    Update About dialog branding
    Changelog:
    
    - Bump assembly version to 1.4.0.0.
    
    - Replace About dialog credits with stfx, FreelancerAftermath, DiscoveryGC, and Groshlancer lines.
    
    - Add Groshlancer 2026 copyright metadata.
    
    - Rework About dialog bounds so title, credits, and dedication do not clip.
    
    - Show the compact FreelancerModStudio product title and Freelancer community dedication.

--- FreelancerModStudio/frmAbout.cs
+++ FreelancerModStudio/frmAbout.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using FreelancerModStudio.Properties;
 
@@ -10,12 +11,33 @@ namespace FreelancerModStudio
         {
             InitializeComponent();
             Helper.UI.ApplyFont(this);
+            FixLayout();
 
             Text = string.Format(Strings.AboutText, Helper.Assembly.Name);
-            lblProductName.Text = Helper.Assembly.Name;
+            lblProductName.Text = "FreelancerModStudio";
             lblVersion.Text = string.Format(Strings.AboutVersion, Helper.Assembly.Version);
-            lblCopyright.Text = Helper.Assembly.Copyright;
-            lblCompanyName.Text = Helper.Assembly.Company;
+            lblCopyright.Text = "Copyright © stfx 2009 - 2013;\r\n" +
+                                "Copyright © FreelancerAftermath 2020;\r\n" +
+                                "Copyright © DiscoveryGC 2024;\r\n" +
+                                "Copyright © Groshlancer 2026";
+            lblCompanyName.Text = string.Empty;
+            txtDescription.Text = "Dedicated to the Freelancer community";
+        }
+
+        void FixLayout()
+        {
+            ClientSize = new Size(480, 320);
+
+            logoPictureBox.SetBounds(0, 0, 165, 320);
+            panel1.SetBounds(165, 0, 315, 92);
+
+            lblProductName.SetBounds(190, 35, 270, 30);
+            lblVersion.SetBounds(190, 112, 270, 18);
+            lblCopyright.SetBounds(190, 132, 270, 78);
+            lblCompanyName.Visible = false;
+
+            txtDescription.SetBounds(190, 240, 270, 36);
+            btnOK.SetBounds(380, 285, 75, 23);
         }
     }
 }
